c99b6a2036e3a6b87ef6844b6666e864809c007e
Web1017 <web1017@hotmail.com>
2016-12-07 12:44:40 -0500

    Add project files.

---
+++ MVC_Project_Part2/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Project_Part2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            //ViewBag.Message = "Your application description page.";
+
+            //return View();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Contact()
+        {
+            //ViewBag.Message = "Stay connected!";
+
+            //return View();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MVC_Project_Part2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_Project_Part2.Startup))]
+namespace MVC_Project_Part2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

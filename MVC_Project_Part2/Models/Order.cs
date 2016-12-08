1acaddfbbbf9f625cbc763c943e50561b6c4f083
mudrakpatel <mudrakpatel96@gmail.com>
2016-12-08 00:26:58 -0600

    [Required(ErrorMessage = "field is required")] added to applicable places inOrder Model

--- MVC_Project_Part2/Models/Order.cs
+++ MVC_Project_Part2/Models/Order.cs
@@ -17,30 +17,39 @@ namespace MVC_Project_Part2.Models
         [StringLength(20)]
         public string Username { get; set; }
 
+        [Required(ErrorMessage = "Firstname is required")]
         [StringLength(20)]
         public string Firstname { get; set; }
 
+        [Required(ErrorMessage = "Lastname is required")]
         [StringLength(20)]
         public string Lastname { get; set; }
 
+        [Required(ErrorMessage = "Address is required")]
         [StringLength(150)]
         public string Address { get; set; }
 
+        [Required(ErrorMessage = "City is required")]
         [StringLength(15)]
         public string City { get; set; }
 
+        [Required(ErrorMessage = "State is required")]
         [StringLength(15)]
         public string State { get; set; }
 
+        [Required(ErrorMessage = "Postal code is required")]
         [StringLength(10)]
         public string Postalcode { get; set; }
 
+        [Required(ErrorMessage = "Country is required")]
         [StringLength(20)]
         public string Country { get; set; }
 
+        [Required(ErrorMessage = "Phone number is required")]
         [StringLength(15)]
         public string Phone { get; set; }
 
+        [Required(ErrorMessage = "Email ID is required")]
         [StringLength(20)]
         public string Email { get; set; }
 

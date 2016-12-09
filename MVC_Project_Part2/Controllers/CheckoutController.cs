9bd4e255f9d8c4c1491a6eeb93da38b126a9a245
mudrakpatel <mudrakpatel96@gmail.com>
2016-12-08 22:02:32 -0500

    Scaffolded AddressAndPayment View

--- MVC_Project_Part2/Controllers/CheckoutController.cs
+++ MVC_Project_Part2/Controllers/CheckoutController.cs
@@ -3,15 +3,81 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+//Refereence to the Models folder
+using MVC_Project_Part2.Models;
 
 namespace MVC_Project_Part2.Controllers
 {
+    [Authorize]
     public class CheckoutController : Controller
     {
-        // GET: Checkout
-        public ActionResult Index()
+        private MenuContext db = new MenuContext();
+        private const string PromoCode = "FREE";
+
+        //
+        // GET: /Checkout/AddressAndPayment
+        public ActionResult AddressAndPayment()
         {
             return View();
         }
+
+        //
+        // POST: /Checkout/AddressAndPayment
+        [HttpPost]
+        public ActionResult AddressAndPayment(FormCollection values)
+        {
+            var order = new Order();
+            TryUpdateModel(order);
+
+            try
+            {
+                if (string.Equals(values["PromoCode"], PromoCode,
+                    StringComparison.OrdinalIgnoreCase) == false)
+                {
+                    return View(order);
+                }
+                else
+                {
+                    order.Username = User.Identity.Name;
+                    order.Orderdate = DateTime.Now;
+
+                    //Save Order
+                    db.Orders.Add(order);
+                    db.SaveChanges();
+                    //Process the order
+                    var cart = ShoppingCart.GetCart(this.HttpContext);
+                    cart.CreateOrder(order);
+
+                    return RedirectToAction("Complete",
+                        new { id = order.OrderID });
+                }
+            }
+            catch
+            {
+                //Invalid - redisplay with errors
+                return View(order);
+            }
+        }
+
+        //
+        // GET: /Checkout/Complete
+        public ActionResult Complete(int id)
+        {
+            // Validate customer owns this order
+            bool isValid = db.Orders.Any(
+                o => o.OrderID == id &&
+                o.Username == User.Identity.Name);
+
+            if (isValid)
+            {
+                return View(id);
+            }
+            else
+            {
+                return View("Error");
+            }
+        }
+
+
     }
 }

9e7d93a00959f53fd351d8d2200f1bfdad17a9e0
mudrakpatel <mudrakpatel96@gmail.com>
2016-12-17 05:49:47 -0500

    Completed ShoppingCaartController

---
+++ MVC_Project_Part2/Controllers/ShoppingCartController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Project_Part2.Models;
+using MVC_Project_Part2.ViewModels;
+
+namespace MVC_Project_Part2.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        MenuContext db = new MenuContext();
+        //
+        // GET: /ShoppingCart/
+        public ActionResult Index()
+        {
+            var cart = ShoppingCart.GetCart(this.HttpContext);
+
+            // Set up our ViewModel
+            var viewModel = new ShoppingCartViewModel
+            {
+                CartItems = cart.GetCartItems(),
+                CartTotal = cart.GetTotal()
+            };
+            // Return the view
+            return View(viewModel);
+        }
+        //
+        // GET: /Store/AddToCart/5
+        public ActionResult AddToCart(int id)
+        {
+            // Retrieve the item from the database
+            var addedAlbum = db.Menu_List
+                .Single(menuItem => menuItem.ItemId == id);
+
+            // Add it to the shopping cart
+            var cart = ShoppingCart.GetCart(this.HttpContext);
+
+            cart.AddToCart(addedAlbum);
+
+            // Go back to the main store page for more shopping
+            return RedirectToAction("Index");
+        }
+        //
+        // AJAX: /ShoppingCart/RemoveFromCart/5
+        [HttpPost]
+        public ActionResult RemoveFromCart(int id)
+        {
+            // Remove the item from the cart
+            var cart = ShoppingCart.GetCart(this.HttpContext);
+
+            //Get the name of the menuItem to display confirmation
+            string albumName = db.Carts
+                .Single(item => item.ItemId == id).ToString();
+
+            // Remove from cart
+            int itemCount = cart.RemoveFromCart(id);
+
+            // Display the confirmation message
+            var results = new ShoppingCartRemoveViewModel
+            {
+                Message = Server.HtmlEncode(albumName) +
+                    " has been removed from your shopping cart.",
+                CartTotal = cart.GetTotal(),
+                CartCount = cart.GetCount(),
+                ItemCount = itemCount,
+                DeleteId = id
+            };
+            return Json(results);
+        }
+        //
+        // GET: /ShoppingCart/CartSummary
+        [ChildActionOnly]
+        public ActionResult CartSummary()
+        {
+            var cart = ShoppingCart.GetCart(this.HttpContext);
+
+            ViewData["CartCount"] = cart.GetCount();
+            return PartialView("CartSummary");
+        }
+    }
+}

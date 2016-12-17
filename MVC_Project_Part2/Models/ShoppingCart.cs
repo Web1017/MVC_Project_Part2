c6a578c76e2f16200619a991ad64ef74f6357250
mudrakpatel <mudrakpatel96@gmail.com>
2016-12-17 02:58:39 -0500

    Added a using statement to ShoppingCart.cs

--- MVC_Project_Part2/Models/ShoppingCart.cs
+++ MVC_Project_Part2/Models/ShoppingCart.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace MVC_Project_Part2.Models
 {
     public class ShoppingCart
     {
+        private MenuContext db = new MenuContext();
     }
 }

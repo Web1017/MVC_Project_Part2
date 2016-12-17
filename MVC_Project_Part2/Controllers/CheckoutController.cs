8574ca39f752c9e128d95dc4e8410ecb0f7f3caa
mudrakpatel <mudrakpatel96@gmail.com>
2016-12-17 07:44:35 -0500

    Added AddressAndPayment View in Checkout View folder

---
+++ MVC_Project_Part2/Controllers/CheckoutController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Project_Part2.Models;
+using MVC_Project_Part2.ViewModels;
+
+namespace MVC_Project_Part2.Controllers
+{
+    [Authorize]
+    public class CheckoutController : Controller
+    {
+        MenuContext db = new MenuContext();
+        const string PromoCode = "FREE";
+
+        // GET: /Checkout/AddressAndPayment
+        public ActionResult AddressAndPayment()
+        {
+            return View();
+        }
+        //
+        // POST: /Checkout/AddressAndPayment
+        [HttpPost]
+        public ActionResult AddressAndPayment(FormCollection values)
+        {
+            var order = new Order();
+            TryUpdateModel(order);
+
+            try
+            {
+                if (string.Equals(values["PromoCode"], PromoCode,
+                    StringComparison.OrdinalIgnoreCase) == false)
+                {
+                    return View(order);
+                }
+                else
+                {
+                    order.Username = User.Identity.Name;
+                    order.Orderdate = DateTime.Now;
+
+                    //Save Order
+                    db.Orders.Add(order);
+                    db.SaveChanges();
+                    //Process the order
+                    var cart = ShoppingCart.GetCart(this.HttpContext);
+                    cart.CreateOrder(order);
+
+                    return RedirectToAction("Complete",
+                        new { id = order.OrderId });
+                }
+            }
+            catch
+            {
+                //Invalid - redisplay with errors
+                return View(order);
+            }
+        }
+        //
+        // GET: /Checkout/Complete
+        public ActionResult Complete(int id)
+        {
+            // Validate customer owns this order
+            bool isValid = db.Orders.Any(
+                o => o.OrderId == id &&
+                o.Username == User.Identity.Name);
+
+            if (isValid)
+            {
+                return View(id);
+            }
+            else
+            {
+                return View("Error");
+            }
+        }
+
+    }
+}
